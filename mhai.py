b07670bf96ac1f3af6cbe6f952ef58bc25956bdb
Yhurie15 <87879775+Yhurie15@users.noreply.github.com>
2021-08-23 13:20:27 +0800

    Update mhai.py

--- mhai.py
+++ mhai.py
@@ -16,8 +16,7 @@ MSG = 'PHTUNNELER'
 COR = '<font color="blue">'
 FTAG = '</font>'
 DEFAULT_HOST = '0.0.0.0:22'
-RESPONSE = "HTTP/1.1 101 MHAILA ROLLAN\r\nContent-Length: 1048576000000\r\n\r\n"
- 
+RESPONSE = "HTTP/1.1 101 MHAILA ROLLAN
 class Server(threading.Thread):
     def __init__(self, host, port):
         threading.Thread.__init__(self)
